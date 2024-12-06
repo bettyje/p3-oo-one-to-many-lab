c712dbf8f059d4dc8c3225fdeac626323740e365
bettyje <bettyjelagat156@gmail.com>
2024-12-06 22:30:56 +0300

    relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(
+                f"Invalid pet_type: {pet_type}. Must be one of {self.PET_TYPES}.")
+        self.name = name
+        self.pet_type = pet_type
+        self._owner = None
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception(" Must be an instance of Owner or None.")
+        self._owner = owner
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("pet instance is required")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
